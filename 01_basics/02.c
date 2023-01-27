11103e061f69735ae0f6d973d5f9e8e572ed95b7
bukharisaidh <bukharisaidh@gmail.com>
2023-01-27 14:48:30 +0300

    added data types

---
+++ 01_basics/02.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+void main(){
+    // 
+    int a = 25;
+    printf("%i \n", a);
+
+    // 
+    double x = 2500.25;
+    printf("%.2lf \n", x);
+
+    // 
+    char h = 'J';
+    printf("%c \n", h);
+
+    // 
+    char name[10] = "Drongo";
+    printf("%s \n", name);
+
+
+
+}
