9e5b8311bd397f65535ddd31954c57d409bc4758
bukharisaidh <bukharisaidh@gmail.com>
2023-01-30 15:54:30 +0300

    if / else

---
+++ 01_basics/04.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void main()
+{
+    char grade1 = 'A';
+    char grade2 = 'B';
+    char grade3 = 'C';
+    printf("drongo student grade numbers \n");
+
+    printf("ALI %c \n", grade1);
+    printf("JOHN %C \n", grade2);
+    printf("MUSES %c \n", grade3);
+
+
+    if (grade2 == 'A')
+    {
+        printf("grade \n");
+    }
+    else {
+        printf(" not grade \n");
+    }
+    
+
+
+
+
+}
